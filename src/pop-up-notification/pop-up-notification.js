c25d07b3834511d665124068e81136fb41292a54
Alina Moskalenko <alina.kravchenko@ukr.net>
2019-01-28 19:14:42 +0200

    Deleted old file

--- src/pop-up-notification/pop-up-notification.js
+++
@@ -1,1 +0,0 @@
-import './pop-up-notification.scss';
--- src/student_page/scripts/notificationMenu.js
+++
@@ -1,7 +0,0 @@
-export function notificationMenu(notificationBtn) {
-    const notification = notificationBtn.querySelector('.notification__conteiner');
-    notificationBtn.addEventListener('click', () => {
-        notification.classList.toggle('notification__conteiner_opened');
-    });
-
-}
